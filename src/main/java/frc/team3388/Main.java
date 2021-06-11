2574702244c15d78436519ade57a9a005ca3b75a
liaarbel <71096174+liaarbel@users.noreply.github.com>
2021-06-11 17:50:42 +0300

    Update Main.java

--- src/main/java/frc/team3388/Main.java
+++ src/main/java/frc/team3388/Main.java
@@ -7,10 +7,15 @@ import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
 import edu.wpi.cscore.VideoSource;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -19,6 +24,22 @@ public class Main {
         // we just connect to it.
         NetworkTableInstance networkTableInstance = NetworkTableInstance.getDefault();
         networkTableInstance.startClientTeam(3388);
+
+        NetworkTable table = networkTableInstance.getTable("vision-color");
+
+        NetworkTableEntry minHue = table.getEntry("minHue");
+        NetworkTableEntry minSat = table.getEntry("minSat");
+        NetworkTableEntry minVal = table.getEntry("minVal");
+        NetworkTableEntry maxHue = table.getEntry("maxHue");
+        NetworkTableEntry maxSat = table.getEntry("maxSat");
+        NetworkTableEntry maxVal = table.getEntry("maxVal");
+        NetworkTableEntry distance = table.getEntry("distance");
+        minHue.setDouble(0.0);
+        minSat.setDouble(44.0);
+        minVal.setDouble(103.0);
+        maxHue.setDouble(95.0);
+        maxSat.setDouble(255.0);
+        maxVal.setDouble(255.0);
 
         // start camera (.close() will be called when the try ends).
         // CvSink allows us to grab images from the camera
@@ -53,7 +74,42 @@ public class Main {
 
                 // TODO: VISION CODE
                 // the image from the camera is in the Mat image
+                Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2HSV);
 
+                int mineHueValue = (int) minHue.getDouble(0.0);
+                int minSatValue = (int) minSat.getDouble(0.0);
+                int minValValue = (int) minVal.getDouble(0.0);
+                int maxHueValue = (int) maxHue.getDouble(0.0);
+                int maxSatValue = (int) maxSat.getDouble(0.0);
+                int maxValValue = (int) maxVal.getDouble(0.0);
+                Core.inRange(image,
+                        new Scalar(mineHueValue, minSatValue, minValValue),
+                        new Scalar(maxHueValue, maxSatValue, maxValValue),
+                        image);
+
+                List<MatOfPoint> contours = new ArrayList<>();
+                Imgproc.findContours(image, contours, new Mat(), Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+
+                Imgproc.cvtColor(image, image, Imgproc.COLOR_GRAY2RGB);
+
+                contours.removeIf((contour) -> contour.total() < 10);
+                for (int i = 0; i < contours.size(); i++) {
+                    MatOfPoint contour = contours.get(i);
+                    Imgproc.drawContours(image, contours, i, new Scalar(255, 57, 0), 2);
+
+                    Rect rect = Imgproc.boundingRect(contour);
+                    if (1.0 - rect.width / rect.height > Math.abs(0.5)) {
+                        continue;
+                    }
+                    Imgproc.rectangle(image, rect.tl(), rect.br(), new Scalar(0, 57, 255), 3);
+
+                    int xCenter = rect.x + rect.width / 2;
+                    int yCenter = rect.y + rect.height / 2;
+
+                    Imgproc.circle(image, new Point(xCenter, yCenter), 3, new Scalar(0, 0, 0), 2);
+
+                    distance.setDouble(xCenter - image.width() / 2);
+                }
 
                 // place the post processed image in the second image
                 // output in CameraServer. That way we
